9d5fa6c44c1b40093116f990889cf347673997d6
GitSabir07 <hasnain.sabir07@gmail.com>
2023-03-02 20:29:01 +0400

    more details added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyDetailsComponent } from './company-details/company-details.component';
@@ -15,6 +14,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from "@angular/material/tabs";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +33,9 @@ import { MatTabsModule } from "@angular/material/tabs";
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatTabsModule
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/company-details/company-details.component.ts
+++ src/app/company-details/company-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-company-details',
@@ -8,11 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyDetailsComponent implements OnInit {
   firstNameAutofilled: boolean | undefined;
   lastNameAutofilled: boolean | undefined
-  constructor() {
+
+  ConsentType: any = ['Yes', 'No', 'NA'];
+  CompanyType: any = ['Main', 'Subsidiary'];
+  CompanyStatus: any = ['NTML', 'TMC', 'OnHold'];
+  EmployerType: any = ['MNC', 'Bank', 'Govt'];
+  Category: any = ['A', 'B', 'T1', 'T2'];
+
+  constructor(public fb: FormBuilder) {
     this.firstNameAutofilled = true;
     this.lastNameAutofilled = false;
   }
-
+  compantDetailsFrom = this.fb.group({
+    name: new FormControl(''),
+    code: new FormControl(''),
+    approvedAuditor: new FormControl(''),
+    tradeLincenseNo: new FormControl(''),
+    website: new FormControl(''),
+    financialsProvider: new FormControl(''),
+    type: new FormControl(''),
+    subsidiary: new FormControl(''),
+    partners: new FormControl(''),
+    status: new FormControl(''),
+    financialsAsPerNorm: new FormControl(''),
+    employerType: new FormControl(''),
+    noOfEmployee: new FormControl(''),
+    formationDate: new FormControl(''),
+    phone: new FormControl(''),
+    poboxno: new FormControl(''),
+    address: new FormControl(''),
+    category: new FormControl('')
+  });
   ngOnInit(): void {
 
   }
